8c398fdc0792fc19ce9da212a785bff084d55adf
Carolina Rodriguez Torres <carolina@endouble.com>
2020-04-13 20:15:17 +0200

    Updates

---
+++ precache-manifest.0dd1bd6ddf1c5bd68fae54bbffb2ad5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe26d91e9a4b7947a2e89372ebd41e89",
+    "url": "/employee-relocator/index.html"
+  },
+  {
+    "revision": "8a833c9b4eba9c15abb8",
+    "url": "/employee-relocator/static/css/main.fdb861de.chunk.css"
+  },
+  {
+    "revision": "4c4afa820cc29c6678d0",
+    "url": "/employee-relocator/static/js/2.1168658d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/employee-relocator/static/js/2.1168658d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a833c9b4eba9c15abb8",
+    "url": "/employee-relocator/static/js/main.93fd2d8b.chunk.js"
+  },
+  {
+    "revision": "16b85547768a9e0ce4af",
+    "url": "/employee-relocator/static/js/runtime-main.f506b187.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/employee-relocator/static/media/logo.5d5d9eef.svg"
+  }
+]);
